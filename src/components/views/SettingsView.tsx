33564fdcc5f3f527aa42d168335394be4e53edbd
Martina Manuel <msmartinamanuel@gmail.com>
2024-08-14 08:33:53 +0800

    Added theme card in settings view

--- src/components/views/SettingsView.tsx
+++ src/components/views/SettingsView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Bell, Globe, Shield } from 'lucide-react';
+import { Bell, Globe, Shield, SunMoon, Sun, Moon } from 'lucide-react';
 
 interface Settings {
   notifications: {
@@ -8,7 +8,8 @@ interface Settings {
     sms: boolean
   },
   language: string,
-  twoFactor: boolean
+  twoFactor: boolean,
+  dark: boolean
 }
 
 type NotificationType = 'email' | 'push' | 'sms';
@@ -22,6 +23,7 @@ function SettingsView() {
     },
     language: 'en',
     twoFactor: false,
+    dark: false //default mode
   });
 
   const handleNotificationChange = (typeString: string) => {
@@ -48,6 +50,13 @@ function SettingsView() {
       twoFactor: !prevSettings.twoFactor,
     }));
   };
+
+  const handleThemeChange = () => {
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      dark: !prevSettings.dark,
+    }))
+  }
 
   return (
     <div className="space-y-6">
@@ -125,6 +134,33 @@ function SettingsView() {
                     settings.twoFactor ? 'translate-x-5' : 'translate-x-0'
                   } pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
                 />
+              </button>
+            </dd>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900 flex items-center">
+            <SunMoon className="mr-2" size={20} /> Theme
+          </h3>
+        </div>
+        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Dark Mode</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <button
+                onClick={handleThemeChange}
+                className={`${
+                  settings.dark ? 'bg-blue-600' : 'bg-gray-200'
+                } relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+              >
+                <span
+                  className={`${
+                    settings.dark ? 'translate-x-5' : 'translate-x-0'
+                  } pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
+                >
+                </span>
               </button>
             </dd>
           </div>
